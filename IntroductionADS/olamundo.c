4ce3616e8e4f5936339108082342030c41fec63c
Victor Bitencourt <victorhugo_biten@hotmail.com>
2025-07-11 13:09:57 -0400

    entry/back data

--- IntroductionADS/olamundo.c
+++ IntroductionADS/olamundo.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
 int main () {
-  printf("Olá, Mundo!\n");
+  int idade;
+  float altura;
+  char nome[20];
+
+  printf("Digite seu nome: ");
+  scanf("%s", nome);
+
+  printf("Digite sua idade: ");
+  scanf("%d", &idade);
+
+  printf("Digite sua altura: ");
+  scanf("%f", &altura);
+
+  printf("O nome é: %s\n", nome);
+  printf("A idade é: %d\n", idade);
+  printf("A altura é: %f\n", altura);
 }
